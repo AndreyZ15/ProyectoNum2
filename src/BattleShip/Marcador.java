67d3476fc2a4764e7f8b4bb5f85458966ea4bac9
AndreyZ15 <andrey.zamora2@ulatina.net>
2023-12-16 13:37:10 -0600

    clase juego, Marcador y Rectangulo

---
+++ src/BattleShip/Marcador.java
@@ -0,0 +1,57 @@
+package BattleShip;
+import java.awt.*;
+
+public class Marcador extends Rectangle {
+    private final Color Golpeado = new Color(219, 23, 23, 180);
+
+    private final Color Fallado = new Color(26, 26, 97, 180);
+
+    //Redondea las esquinas para que llene el rectangulo un poco menos
+    private final int Redondeo = 3;
+
+    private boolean verMarca;
+
+    private Ship barcoMarcado;
+
+    public Marker(int x, int y, int width, int height) {
+        super(x, y, width, height);
+        reset();
+    }
+
+    public void reset() {
+        barcoMarcado = null;
+        verMarca = false;
+    }
+
+    public void mark() {
+        if(!verMarca && esBarco()) {
+            barcoMarcado.destruirSeccion();
+        }
+        verMarca = true;
+    }
+
+    public boolean Marca() {
+        return verMarca;
+    }
+
+    public void setBarco(Barco barco) {
+        this.barcoMarcado = barco;
+    }
+
+    public boolean esBarco() {
+        return barcoMarcado != null;
+    }
+
+    public Barco getBarcoCerca() {
+        return barcoMarcado;
+    }
+
+    public void pintura(Graphics g) {
+        if(!verMarca) return;
+
+        g.setColor(esBarco() ? Golpeado : Fallado);
+        g.fillRect(posicion.x+Redondeo+1, posicion.y+Redondeo+1, width-Redondeo*2, height-Redondeo*2);
+    }
+
+
+}
---
+++ src/BattleShip/Rectangulo.java
@@ -0,0 +1,37 @@
+package BattleShip;
+
+
+
+public class Rectangulo {
+    protected Posicion posicion;
+
+    protected int ancho;
+
+    protected int altura;
+
+    public Rectangulo(Posicion posicion, int ancho, int altura) {
+        this.posicion = posicion;
+        this.ancho = ancho;
+        this.altura = altura;
+    }
+    public Rectangulo(int x, int y, int ancho, int altura) {
+        this(new Posicion(x,y),ancho,altura);
+    }
+
+    public int getAncho() {
+        return ancho;
+    }
+    public int getAltura() {
+        return altura;
+    }
+
+    public Posicion getPosicion() {
+        return posicion;
+    }
+
+    public boolean esPosicion(Posicion targetPosicion) {
+        return targetPosicion.x >= posicion.x && targetPosicion.y >= posicion.y
+                && targetPosicion.x < posicion.x + ancho && targetPosicion.y < position.y + altura;
+    }
+
+}
