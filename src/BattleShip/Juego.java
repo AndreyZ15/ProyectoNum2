a7a72d03757c2b8897f39b79b66bd053cbc4c6ed
Antonio! <antonyajar095@gmail.com>
2023-12-19 14:05:23 -0600

    agreg0 clase 5.0 si no devolverse

--- src/BattleShip/Juego.java
+++ src/BattleShip/Juego.java
@@ -2,14 +2,18 @@ package BattleShip;
 import javax.swing.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Juego implements KeyListener {
-
-    public static void main(String[] args) {
-        Juego juego = new Juego();
-    }
+    private static final int PUERTO = 12345; // Puerto para la comunicación
 
     private GamePanel gamePanel;
+
+    private Socket socket;
+    private ObjectOutputStream outputStream;
+    private ObjectInputStream inputStream;
 
     public Juego() {
         // Seleccionar la dificultad
@@ -30,10 +34,75 @@ public class Juego implements KeyListener {
         frame.addKeyListener(this);
         frame.pack();
         frame.setVisible(true);
+
+        if (setupMultijugador()) {
+            // Iniciar el hilo para manejar la comunicación del jugador
+            Thread thread = new Thread(this::manejarComunicacion);
+            thread.start();
+        }
     }
+    private boolean setupMultijugador() {
+        String[] options = new String[]{"Crear partida", "Unirse a partida"};
+        int choice = JOptionPane.showOptionDialog(null, "Seleccione una opción para el multijugador",
+                "Multijugador", JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE,
+                null, options, options[0]);
+
+        if (choice == 0) { // Crear partida
+            try {
+                ServerSocket serverSocket = new ServerSocket(PUERTO);
+                System.out.println("Esperando a que se conecte un jugador...");
+                socket = serverSocket.accept();
+                System.out.println("Jugador conectado");
+            } catch (IOException e) {
+                e.printStackTrace();
+                return false;
+            }
+        } else if (choice == 1) { // Unirse a partida
+            String ip = JOptionPane.showInputDialog("Introduzca la dirección IP del servidor:");
+            try {
+                socket = new Socket(ip, PUERTO);
+                System.out.println("Conectado al servidor");
+            } catch (IOException e) {
+                e.printStackTrace();
+                return false;
+            }
+        } else {
+            return false;
+        }
+
+        try {
+            outputStream = new ObjectOutputStream(socket.getOutputStream());
+            inputStream = new ObjectInputStream(socket.getInputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+
+        return true;
+    }
+
+    private void manejarComunicacion() {
+        try {
+            while (true) {
+                // Manejar la comunicación con el otro jugador aquí
+                // Puedes usar outputStream.writeObject() y inputStream.readObject() para enviar y recibir datos
+            }
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
     @Override
     public void keyPressed(KeyEvent e) {
         gamePanel.handleInput(e.getKeyCode());
+        if (outputStream != null) {
+            try {
+                outputStream.writeInt(e.getKeyCode());
+                outputStream.flush();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
     }
 
     @Override
---
+++ src/BattleShip/Main.java
@@ -0,0 +1,7 @@
+package BattleShip;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Juego juego = new Juego();
+    }
+}
